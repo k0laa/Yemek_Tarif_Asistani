56f7af657831437377a6d328435b3e178e82f116
bugra <bugrayalcn1@gmail.com>
2024-12-15 21:04:06 +0300

    feat: Added translator class
    added deepl library to translate output from English to other languages

---
+++ DeepL.py
@@ -0,0 +1,8 @@
+import deepl
+
+auth_key = "c0c9ec71-49cc-4b12-9b57-f7168f9a9399:fx"  ##aylık 500.000 karakter
+translator = deepl.Translator(auth_key)
+
+def translate(text, source_lang, target_lang):
+    response = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+    return response
