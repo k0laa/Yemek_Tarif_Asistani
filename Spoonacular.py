32bb5c82452ce234d6a14c2bde4b978994cac8fa
bugra <bugrayalcn1@gmail.com>
2024-12-19 00:13:16 +0300

    feat : api keys have been increased and hidden
    The quota of the system was multiplied by increasing the free api keys.

--- Spoonacular.py
+++ Spoonacular.py
@@ -2,31 +2,56 @@ import requests
 import DeepL as dl
 from bs4 import BeautifulSoup
 
-api_key = "83b2b1202ace49ebb26496ac998fedb4"  ##günlük kota 150
 base_url = "https://api.spoonacular.com/recipes/"
 
 
-# Gelen malzemerlere göre 5 tane tarif bulur
 def find_recipe(ingredients):
+    api_keys = load_api_keys()
+    for api_key in api_keys:
+        # API ile tarif aramasını dene
+        recipes = get_recipe_from_api(api_key, ingredients)
+        if recipes is not None:
+            return recipes
+
+    return None
+
+
+def get_recipe_details(recipe_id):
+    api_keys = load_api_keys()
+    for api_key in api_keys:
+        # API ile tarif aramasını dene
+        details = get_recipe_details_from_api(api_key, recipe_id)
+        if details is not None:
+            return details
+
+    return None
+
+
+# Gelen malzemerlere göre 4 tane tarif bulur
+def get_recipe_from_api(api_key, ingredients):
     ingredients_en = dl.translate(ingredients, 'TR', 'EN-US')
-    # API'ye tarif bulma isteği gönder
-    search_url = f"{base_url}findByIngredients"  #
+    search_url = f"{base_url}findByIngredients"
     params = {
         "ingredients": ingredients_en,
-        "number": 4,  # Gösterilecek tarif sayısı
+        "number": 4,
         "apiKey": api_key
     }
     response = requests.get(search_url, params=params)
-    recipes = response.json()
-    return recipes
+
+    # Kota bittiğinde kontrol
+    if response.status_code == 402:
+        return None
+    return response.json()
 
 
 # ID si gelen tarifin detaylarını getirir
-def get_recipe_details(recipe_id):
+def get_recipe_details_from_api(api_key, recipe_id):
     details_url = f"{base_url}{recipe_id}/information"
     response = requests.get(details_url, params={"apiKey": api_key})
-    recipe_details = response.json()
-    return recipe_details
+
+    if response.status_code == 402:
+        return None
+    return response.json()
 
 
 # Bulanan tarifleri konsola yazdırır (main.py için)
@@ -55,3 +80,9 @@ def print_recipe_details(recipe_details):
     instructions_text = soup.get_text(separator="\n")  # Her bir maddeyi yeni satırda yazdırır
     instructions_tr = dl.translate(instructions_text, 'EN', 'TR')
     print(instructions_tr)
+
+
+def load_api_keys(filename="SPOONACULAR_APİ_KEYS.txt"):
+    with open(filename, 'r') as file:
+        api_keys = [line.strip() for line in file.readlines()]
+    return api_keys
