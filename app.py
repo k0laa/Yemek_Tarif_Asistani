f9a1cf013c2f3334752cc970d5cf4bb92721e176
bugra <bugrayalcn1@gmail.com>
2024-12-18 23:06:02 +0300

    refactor: reorganised functions due to incompatibility with the data provided by the spoonacular library

--- app.py
+++ app.py
@@ -3,41 +3,62 @@ from flask import Flask, render_template, request
 import Spoonacular as sp
 import DeepL as dl
 
+finded_recipes_details = []
+ingredients = ''
+
 app = Flask(__name__)
 
 
-# tarif arama sayfası
+# Tarif arama sayfası
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# tarif arama sonuçları sayfası
+# Tarif arama sonuçları sayfası
 @app.route('/find_recipe', methods=['POST'])
 def find_recipe():
-    ingredients = request.form['ingredients']  # formdan gelen malzemeleri alır
-    recipes = sp.find_recipe(ingredients)  # malzemelere göre tarif arar
-    for recipe in recipes:
-        recipe['title'] = dl.translate(recipe['title'], 'EN', 'TR')  # tarif adını çevirir
+    global finded_recipes_details, ingredients
 
-    return render_template('recipes.html', recipes=recipes)
+    ingredients_form = request.form['ingredients']  # Formdan gelen malzemeleri alır
+
+    if ingredients_form != ingredients:  # sayfa yenilendiğinde aynı malzemeleri tekrar aramamak için
+        finded_recipes_details.clear()
+        ingredients = ingredients_form
+        recipes = sp.find_recipe(ingredients)  # Malzemelere göre tarif arar
+        for recipe in recipes:  #
+            details = sp.get_recipe_details(recipe['id'])  # Tarif detaylarını getirir
+            details['title'] = dl.translate(details['title'], 'EN', 'TR')  # Detaylardaki başlığı çevirir
+            finded_recipes_details.append(details)
+
+    return render_template('recipes.html', recipes=finded_recipes_details)
 
 
-# tarif detayları sayfası
+# Tarif detayları sayfası
 @app.route('/recipe_details/<int:recipe_id>')
 def recipe_details(recipe_id):
-    # tarif detaylarını getirir
-    recipe_details = sp.get_recipe_details(recipe_id)
+    recipe_id = int(recipe_id)
+    global finded_recipes_details
+    recipe_details = None
 
-    # tarif detaylarını çevirir
-    for ingredient in recipe_details['extendedIngredients']:
+    # İlgili tarifin detaylarını bul
+    for recipe in finded_recipes_details:
+        if recipe['id'] == recipe_id:
+            recipe_details = recipe
+            break
+
+    if recipe_details is None:
+        return "Tarif bulunamadı", 404
+
+    # Malzeme açıklamalarını çevir
+    for ingredient in recipe_details.get('extendedIngredients', []):
         ingredient['original'] = dl.translate(ingredient['original'], 'EN', 'TR')
-    recipe_details['title'] = dl.translate(recipe_details['title'], 'EN', 'TR')
 
-    # tarif adımlarını çevirir
-    soup = BeautifulSoup(recipe_details['instructions'], "html.parser")
-    instructions_text = soup.get_text(separator="\n")  # Her bir maddeyi yeni satırda yazdırır
-    recipe_details['instructions'] = dl.translate(instructions_text, 'EN', 'TR')
+    # Tarif adımlarını çevir
+    if 'instructions' in recipe_details and recipe_details['instructions']:
+        soup = BeautifulSoup(recipe_details['instructions'], "html.parser")
+        instructions_text = soup.get_text(separator="\n")
+        recipe_details['instructions'] = dl.translate(instructions_text, 'EN', 'TR')
 
     return render_template('recipe_details.html', recipe_details=recipe_details)
 
