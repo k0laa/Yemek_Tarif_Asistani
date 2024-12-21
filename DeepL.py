955ec5a189823f794265de1ebddfdd2aa8251a22
bugra <bugrayalcn1@gmail.com>
2024-12-21 23:12:29 +0300

    update: no more gives an error when sending empty text

--- DeepL.py
+++ DeepL.py
@@ -4,6 +4,8 @@ auth_key = "c0c9ec71-49cc-4b12-9b57-f7168f9a9399:fx"  ##aylık kota 500.000 kara
 translator = deepl.Translator(auth_key)
 
 def translate(text, source_lang, target_lang):
+    if text is None or text == "":
+        return None
     response = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
     return response
 
